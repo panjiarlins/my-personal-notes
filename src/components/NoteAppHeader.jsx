3217ffefddc78fac92cf3450abb3886986f05b9b
Panji Arlin Saputra <75957797+panjiarlins@users.noreply.github.com>
2023-04-06 16:21:07 +0700

    Fix app header

--- src/components/NoteAppHeader.jsx
+++ src/components/NoteAppHeader.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
+import NoteSearch from './NoteSearch';
 
 function NoteAppHeader({ onSearch }){
     return (
         <div className="note-app__header">
             <h1>Catatan</h1>
-            <input type="text" placeholder="Cari catatan ...." onKeyUp={event => onSearch(event.target.value)} />
+            <NoteSearch onSearch={onSearch} />
         </div>
     ); 
 }
