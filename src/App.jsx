00f26e0978d838e0570efcb77d0e16d5b2a8a2ab
Panji Arlin Saputra <75957797+panjiarlins@users.noreply.github.com>
2023-04-19 18:53:17 +0700

    Create: NotFoundPage

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import HomePage from './pages/HomePage';
 import ArchivesPage from './pages/ArchivesPage';
 import DetailPage from './pages/DetailPage';
 import AddNewPage from './pages/AddNewPage';
+import NotFoundPage from './pages/NotFoundPage';
 
 function App() {
   return (
@@ -16,6 +17,7 @@ function App() {
                 <Route path='/archives' element={<ArchivesPage />} />
                 <Route path='/notes/:id' element={<DetailPage />} />
                 <Route path='/notes/new' element={<AddNewPage />} />
+                <Route path='*' element={<NotFoundPage />} />
             </Routes>
         </main>
     </div>
