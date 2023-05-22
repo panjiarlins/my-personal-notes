54d4bd9baaa763951ad44e7619599a19ac3c604a
Panji Arlin Saputra <75957797+panjiarlins@users.noreply.github.com>
2023-05-22 13:38:27 +0700

    Fix: hide Archived header link when display ArchivesPage

--- src/components/NoteAppHeader.jsx
+++ src/components/NoteAppHeader.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 import { FiLogOut } from 'react-icons/fi';
 import ToggleTheme from './ToggleTheme';
 import ToggleLocale from './ToggleLocale';
@@ -8,13 +8,15 @@ import LocaleContext from '../contexts/LocaleContext';
 
 function NoteAppHeader({ name, logout }){
     const { locale } = React.useContext(LocaleContext);
+    const location = useLocation();
+    const firstPath = location.pathname.split('/')[1];
 
     return (
         <header>
             <h1><Link to="/">{locale === 'id' ? 'Aplikasi Catatan' : 'Note App'}</Link></h1>
             <nav className="navigation">
                 <ul>
-                    {!!name && <li><Link to="/archives">{locale === 'id' ? 'Terarsip' : 'Archived'}</Link></li>}
+                    {!!name && (firstPath!=='archives') && <li><Link to="/archives">{locale === 'id' ? 'Terarsip' : 'Archived'}</Link></li>}
                     <li><ToggleLocale /></li>
                     <li><ToggleTheme /></li>
                     {!!logout && <li><button className="button-logout" onClick={logout}><FiLogOut /></button></li>}
